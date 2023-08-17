3bb647b8c926e346c010e422bf99526efd271904
Алексей Шубенков <shubenkov-93@yandex.ru>
2023-08-17 19:43:25 +0300

    Unittest в Django: тестирование контента. Тестируем сортировку комментариев на странице новости

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -3,8 +3,13 @@ from django.conf import settings
 from django.test import TestCase
 # Импортируем функцию reverse(), она понадобится для получения адреса страницы.
 from django.urls import reverse
+from django.utils import timezone
+# Импортируем функцию для получения модели пользователя.
+from django.contrib.auth import get_user_model
+# Дополнительно к News импортируем модель комментария.
+from news.models import Comment, News
 
-from news.models import News
+User = get_user_model()
 
 # Импортируйте нужные классы.
 from datetime import datetime, timedelta
@@ -49,3 +54,40 @@ class TestHomePage(TestCase):
         sorted_dates = sorted(all_dates, reverse=True)
         # Проверяем, что исходный список был отсортирован правильно.
         self.assertEqual(all_dates, sorted_dates)
+
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            title='Тестовая новость', text='Просто текст.'
+        )
+        # Сохраняем в переменную адрес страницы с новостью:
+        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
+        cls.author = User.objects.create(username='Комментатор')
+        # Запоминаем текущее время:
+        now = timezone.now()
+        # Создаём комментарии в цикле.
+        for index in range(2):
+            # Создаём объект и записываем его в переменную.
+            comment = Comment.objects.create(
+                news=cls.news, author=cls.author, text=f'Tекст {index}',
+            )
+            # Сразу после создания меняем время создания комментария.
+            comment.created = now + timedelta(days=index)
+            # И сохраняем эти изменения.
+            comment.save()
+
+    def test_comments_order(self):
+        response = self.client.get(self.detail_url)
+        # Проверяем, что объект новости находится в словаре контекста
+        # под ожидаемым именем - названием модели.
+        self.assertIn('news', response.context)
+        # Получаем объект новости.
+        news = response.context['news']
+        # Получаем все комментарии к новости.
+        all_comments = news.comment_set.all()
+        # Проверяем, что время создания первого комментария в списке
+        # меньше, чем время создания второго.
+        self.assertLess(all_comments[0].created, all_comments[1].created)
