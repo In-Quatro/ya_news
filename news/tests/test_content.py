da63d9eb98bf5f658e36040d90c373394024cef1
Алексей Шубенков <shubenkov-93@yandex.ru>
2023-08-17 19:39:31 +0300

    Unittest в Django: тестирование контента. Тестируем сортировку новостей

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -6,15 +6,25 @@ from django.urls import reverse
 
 from news.models import News
 
+# Импортируйте нужные классы.
+from datetime import datetime, timedelta
+
 
 class TestHomePage(TestCase):
-    # Вынесем ссылку на домашнюю страницу в атрибуты класса.
     HOME_URL = reverse('news:home')
 
     @classmethod
     def setUpTestData(cls):
+        # Вычисляем текущую дату.
+        today = datetime.today()
         all_news = [
-            News(title=f'Новость {index}', text='Просто текст.')
+            News(
+                title=f'Новость {index}',
+                text='Просто текст.',
+                # Для каждой новости уменьшаем дату на index дней от today,
+                # где index - счётчик цикла.
+                date=today - timedelta(days=index)
+            )
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
         ]
         News.objects.bulk_create(all_news)
@@ -29,3 +39,13 @@ class TestHomePage(TestCase):
         news_count = len(object_list)
         # Проверяем, что на странице именно 10 новостей.
         self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
+
+    def test_news_order(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        # Получаем даты новостей в том порядке, как они выведены на странице.
+        all_dates = [news.date for news in object_list]
+        # Сортируем полученный список по убыванию.
+        sorted_dates = sorted(all_dates, reverse=True)
+        # Проверяем, что исходный список был отсортирован правильно.
+        self.assertEqual(all_dates, sorted_dates)
