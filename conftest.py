83269cb35ce50f78ab0da66c548e25d4007532e4
Алексей Шубенков <shubenkov-93@yandex.ru>
2023-08-19 17:07:04 +0300

    Pytest для Django. Тестирование маршрутов

---
+++ conftest.py
@@ -0,0 +1,42 @@
+import pytest
+
+from news.models import News, Comment
+
+@pytest.fixture
+def author(django_user_model):
+    return django_user_model.objects.create(username='Автор')
+
+
+@pytest.fixture
+def author_client(author, client):
+    client.force_login(author)
+    return client
+
+
+@pytest.fixture
+def news(author):
+    news = News.objects.create(
+        title='Заголовок',
+        text='Текст'
+    )
+    return news
+
+
+@pytest.fixture
+def comment(news, author):
+    comment = Comment.objects.create(
+        news=news,
+        author=author,
+        text='Текст комментария'
+    )
+    return comment
+
+
+@pytest.fixture
+def id_new_for_args(news):
+    return news.id,
+
+
+@pytest.fixture
+def id_comment_for_args(comment):
+    return comment.id,
